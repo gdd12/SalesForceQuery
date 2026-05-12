7058847ad87c9e7ca220839a4e1a9e4c067fc931
gdd12 <vytalpvp@gmail.com>
2026-05-11 20:06:59 -0700

    Added a logger rollover policy. Default is 2mb for 2 total files.

--- src/logger.py
+++ src/logger.py
@@ -1,11 +1,16 @@
 import logging, sys
 from pathlib import Path
 from utils.variables import FileNames
+from logging.handlers import RotatingFileHandler
 
 LOG_FILE = Path(__file__).resolve().parent.parent / FileNames.RunningLog
 logger = logging.getLogger("logger")
 
-def setup_logger(log_level=None):
+def setup_logger(
+    log_level=None,
+    max_mb=2,
+    backup_count=1
+):
     global logger
     if log_level not in ('info', 'debug'):
         return logger
@@ -19,8 +24,16 @@ def setup_logger(log_level=None):
     formatter_file = logging.Formatter('%(asctime)s - %(levelname)-6s [%(module)s] (%(funcName)s) - %(message)s')
     formatter_console = logging.Formatter('%(message)s')
 
+    max_bytes = max_mb * 1024 * 1024
+
+    file_handler = RotatingFileHandler(
+        LOG_FILE,
+        maxBytes=max_bytes,
+        backupCount=backup_count,
+        encoding="utf-8"
+    )
+
     if not logger.handlers or isinstance(logger.handlers[0], logging.NullHandler):
-        file_handler = logging.FileHandler(LOG_FILE)
         console_handler = logging.StreamHandler(sys.stdout)
         if log_level == 'debug':
             file_handler.setLevel(logging.DEBUG)
