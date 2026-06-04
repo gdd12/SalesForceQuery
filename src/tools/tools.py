912caa96ae0ab63622b4678b610d77c6517082de
gdd12 <vytalpvp@gmail.com>
2026-06-04 12:32:49 -0700

    Reworked the tool module to handle new tooling better.

--- src/tools/tools.py
+++ src/tools/tools.py
@@ -13,62 +13,81 @@ class Tools():
   def __init__(self, config_with_filereg):
     self.config: Config = config_with_filereg
     self.msg = "is connected to the program"
+    self.extras = None
 
   def run(self, type: str, extras=None):
-    if type == None: handle_shutdown(1, reason="Invalid Tool")
+    self.extras = extras
 
-    elif type == VARS.Vacation: result = self.VACATION_TOOL(extras)
-    elif type == VARS.Exclude:  result = self.EXCLUSION_TOOL(extras)
-    elif type == VARS.Clean:    result = self.CLEAN_TOOL()
-    elif type == VARS.Config:   result = self.CONFIG_TOOL()
-    elif type == VARS.Setup:    result = self.SETUP_TOOL()
-    elif type == VARS.Role:     result = self.ROLE_TOOL()
-    elif type == VARS.Team:     result = self.TEAM_TOOL(extras)
-    
-    logger.info(result)
-    print(result)
-    handle_shutdown(exit_code=0)
+    tool_modules = {
+      VARS.Vacation: self.VACATION_TOOL,
+      VARS.Exclude:  self.EXCLUSION_TOOL,
+      VARS.Clean:    self.CLEAN_TOOL,
+      VARS.Config:   self.CONFIG_TOOL,
+      VARS.Setup:    self.SETUP_TOOL,
+      VARS.Role:     self.ROLE_TOOL,
+      VARS.Team:     self.TEAM_TOOL,
+    }
+
+    if type == None:
+      handle_shutdown(1, reason="Invalid Tool")
+
+    tool_result = tool_modules.get(type, lambda: "unkown")()
 
   def CLEAN_TOOL(self):
     tool_name = self.CLEAN_TOOL.__name__
     logger.info(f"{tool_name} is connected to the program")
-    cfg_clean = self.config.clean()
-    passwd_clean = self.config.remove_key_files()
-    vacation_clean = self.config.update_config_value("rules.vacation_scheduled_until", value="")
 
-    return f"{tool_name} completed the clean operation"
+    self.config.clean()
+    self.config.remove_key_files()
+    self.config.update_config_value("rules.vacation_scheduled_until", value="")
+
+    msg = f"{tool_name} completed the clean operation"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
 
   def VACATION_TOOL(self, date):
     tool_name = self.VACATION_TOOL.__name__
     logger.info(f"{tool_name} is connected to the program")
 
     if date and type(calculate_days_delta(date)) != int:
-      return f"Unable to set vacation return date {date} as it is invalid."
+      logger.error(f"Unable to set vacation return date {date} as it is invalid.")
+      handle_shutdown(module=tool_name)
 
     result = self.config.update_config_value(key="rules.vacation_scheduled_until", value=date)
 
-    return f"{tool_name} completed the vacation setup with the date {result}"
+    msg = f"{tool_name} completed the vacation setup with the date {result}"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
   
   def CONFIG_TOOL(self):
     tool_name = self.CONFIG_TOOL.__name__
     logger.info(f"{tool_name} {self.msg}")
     self.config.print_configuration()
 
-    return f"{tool_name} completed the print operation"
+    msg = f"{tool_name} completed the print operation"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
   
-  def EXCLUSION_TOOL(self, extras: dict):
+  def EXCLUSION_TOOL(self):
     tool_name = self.EXCLUSION_TOOL.__name__
     logger.info(f"{tool_name} {self.msg}")
+
+    extras: dict = self.extras
+
     self.config.add_exclusion(extras)
 
-    return f"{tool_name} completed the exclusion of type {str(extras.get('type')).upper()}"
+    msg = f"{tool_name} completed the exclusion of type {str(extras.get('type')).upper()}"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
   
   def SETUP_TOOL(self):
     tool_name = self.SETUP_TOOL.__name__
     logger.info(f"{tool_name} {self.msg}")
     rewrite_configuration()
 
-    return f"{tool_name} completed"
+    msg = f"{tool_name} completed",
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
   
   def SIMULATE_TOOL(self, extras):
     from tools.simulation import simulate
@@ -77,18 +96,24 @@ class Tools():
     logger.info(f"{tool_name} {self.msg}")
     simulate(logger=extras)
 
-    return f"{tool_name} completed the simulation of the query"
+    msg = f"{tool_name} completed the simulation of the query"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
   
   def ROLE_TOOL(self):
     tool_name = self.ROLE_TOOL.__name__
     logger.info(f"{tool_name} {self.msg}")
     new_role = self.config.toggle_role()
 
-    return f"{tool_name} completed the switch to {new_role} role"
+    msg = f"{tool_name} completed the switch to {new_role} role"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
 
-  def TEAM_TOOL(self, extra):
+  def TEAM_TOOL(self):
     tool_name = self.TEAM_TOOL.__name__
     logger.info(f"{tool_name} {self.msg}")
+
+    extra: str = self.extras
 
     filereg = FileReg()
     filereg.init()
@@ -101,4 +126,6 @@ class Tools():
     )
     TeamTool.run()
 
-    return f"{tool_name} completed the {extra.upper()} operation"
+    msg = f"{tool_name} completed the {extra.upper()} operation"
+    logger.info(msg)
+    handle_shutdown(reason=msg, module=tool_name)
